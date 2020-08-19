73c95442ba119c3291058d002d563f7ab113b9a7
shaynasrag <54994003+shaynasrag@users.noreply.github.com>
2020-08-19 13:38:49 -0400

    made averages for each experiment!!!!!!
    SO HELPFUL! yay!

--- experiments/averages.py
+++ experiments/averages.py
@@ -4,19 +4,19 @@ from statistics import mean
 import csv
 
 def main():
-    print("CWD", os.getcwd())
     dicts1 = os.path.join(argv[1], "model-1", "dicts.csv")
     dicts2 = os.path.join(argv[1], "model-2", "dicts.csv")
     dicts3 = os.path.join(argv[1], "model-3", "dicts.csv")
     dicts4 = os.path.join(argv[1], "model-4", "dicts.csv")
     dicts5 = os.path.join(argv[1], "model-5", "dicts.csv")
-    with open(dicts1, 'r') as dicts1file, open(dicts2, 'r') as dicts2file, open(dicts3, 'r') as dicts3file, open(dicts4, 'r') as dicts4file, open(dicts5, 'r') as dicts5file:
+    avgdicts = os.path.join(argv[1], "AverageDicts.csv")
+    with open(dicts1, 'r') as dicts1file, open(dicts2, 'r') as dicts2file, open(dicts3, 'r') as dicts3file, open(dicts4, 'r') as dicts4file, open(dicts5, 'r') as dicts5file, open(avgdicts, 'w') as avgfile:
         dicts1list = list(csv.reader(dicts1file, delimiter='\t', lineterminator='\n'))
         dicts2list = list(csv.reader(dicts2file, delimiter='\t', lineterminator='\n'))
         dicts3list = list(csv.reader(dicts3file, delimiter='\t', lineterminator='\n'))
         dicts4list = list(csv.reader(dicts4file, delimiter='\t', lineterminator='\n'))        
         dicts5list = list(csv.reader(dicts5file, delimiter='\t', lineterminator='\n'))
-        dictslist = [dicts1list, dicts2list, dicts3list, dicts4list, dicts5list]
+        avgwriter = csv.writer(avgfile, delimiter=',', lineterminator='\n')
         dictnum = len(list(dicts1list))
         collatedDicts = []
         for i in range(dictnum):
@@ -32,17 +32,15 @@ def main():
                 for j in range(5):
                     for k in range(len(collatedDicts[i][j])):
                         collatedDicts[i][j][k] = int(collatedDicts[i][j][k])
-        collatedNums= []
+        meanDicts = []
         for i in range(dictnum):
-            for j in range(5):
-                for k in range(len(collatedDicts[i][j])):
-                    
-            mean(collatedDicts[i][j])
-                    
-               
+            meandict = []
+            for j in range(len(collatedDicts[0][0])):
+                meandict.append(mean([collatedDicts[i][0][j], collatedDicts[i][1][j], collatedDicts[i][2][j], collatedDicts[i][3][j], collatedDicts[i][4][j]]))
+            meanDicts.append(meandict)  
+        dictnames = [dicts1list[i][0].split(',')[0] for i in range(dictnum)]
+        for i in range(dictnum):
+            meanDicts[i].insert(0, dictnames[i])
+            avgwriter.writerow(meanDicts[i])
 
-                
-        
-        print('hello')
-        l = 5
 main()
